02be540f928e7223cc68373af9d5e497fb24b4f9
Sakib-Atreus <sakibatreus@gmail.com>
2024-05-21 03:18:14 +0600

    movie flix update

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express, { Request, Response } from 'express';
+import { MovieRoutes } from './modules/movies/movie.route';
 
 const app = express();
 const port = 3000;
 
+//parsers
+app.use(express.json());
+
+app.use("/api/movies", MovieRoutes);
+
 app.get('/', (req : Request, res : Response) => {
-    res.send("Hello World!");
+    res.send("Hey, movie world!");
 })
 
 export default app;
